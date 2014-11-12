060f113e099a566a10d849ae50dde1a567879d04
Peter Müller <munter@fumle.dk>
2014-11-12 16:57:07 +0100

    Added dead option from livestyle

--- tasks/grunt-livestyle.js
+++ tasks/grunt-livestyle.js
@@ -18,6 +18,7 @@ module.exports = function (grunt) {
                 cssImages: config.watchCssImages === false ? false : true,
                 processimage: config.processimage === false ? false : true,
                 debug: !!config.debug,
+                dead: !!config.dead,
                 mappings: config.mappings || {}
             };
 
